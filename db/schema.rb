77e281fd9fe5bb1e97b09732f8528d23e84acf8d
Denilson Telaroli <ddts80@gmail.com>
2015-11-14 00:18:37 -0200

    add owner

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151109221652) do
+ActiveRecord::Schema.define(version: 20151114001939) do
+
+  create_table "owners", force: :cascade do |t|
+    t.string   "provider",               default: "email", null: false
+    t.string   "uid",                    default: "",      null: false
+    t.string   "encrypted_password",     default: "",      null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,       null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.string   "name"
+    t.string   "nickname"
+    t.string   "image"
+    t.string   "email"
+    t.text     "tokens"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "owners", ["email"], name: "index_owners_on_email"
+  add_index "owners", ["reset_password_token"], name: "index_owners_on_reset_password_token", unique: true
+  add_index "owners", ["uid", "provider"], name: "index_owners_on_uid_and_provider", unique: true
 
   create_table "users", force: :cascade do |t|
     t.string   "provider",               default: "email", null: false
