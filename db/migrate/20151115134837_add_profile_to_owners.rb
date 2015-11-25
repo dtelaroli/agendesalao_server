72362773d5a5a7ef0fb8a521775f6fe39a2d136a
Denilson Telaroli <ddts80@gmail.com>
2015-11-25 21:15:46 -0200

    Time per client

--- db/migrate/20151115134837_add_profile_to_owners.rb
+++
@@ -1,5 +0,0 @@
-class AddProfileToOwners < ActiveRecord::Migration
-  def change
-    add_reference :owners, :profile, index: true, foreign_key: true
-  end
-end
--- db/migrate/20151122194800_add_data_to_owners.rb
+++ db/migrate/20151122194800_add_data_to_owners.rb
@@ -1,5 +1,7 @@
 class AddDataToOwners < ActiveRecord::Migration
   def change
+    add_reference :owners, :profile, index: true, foreign_key: true
+    
     add_column :owners, :mon, :boolean, default: false
     add_column :owners, :tue, :boolean, default: false
     add_column :owners, :wed, :boolean, default: false
